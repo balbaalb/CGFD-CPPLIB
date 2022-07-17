23e48ad6b5c39f452b3a0bda5ea11c6f869652fe
Bashar Albaalbaki <bashar.albaalbaki@gmail.com>
2022-07-17 09:58:16 -0600

    BitMap generator from John Purcell's udemy advanced CPP course.

---
+++ BitMap.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+//#include "BitMapFileHeader.h"
+//#include "BitMapInfoHeader.h"
+#include "BitMap.h"
+bBitMap::BitMap::BitMap(int width, int height) : Width(width), Height(height), PixelsBites(width* height * 3), Pixels(new uint8_t[PixelsBites]{})
+{
+	//Create a White Canvas
+	for (int i = 0; i < Width; ++i)
+	{
+		for (int j = 0; j < Height; ++j)
+		{
+			this->Draw1Pixel(i, j, 255, 255, 255);
+		}
+	}
+}
+bBitMap::BitMap::~BitMap()
+{
+}
+void bBitMap::BitMap::Draw1Pixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue)
+{
+	uint8_t* pPixel = Pixels.get() + (y * Width + x) * 3;
+	pPixel[0] = blue;
+	pPixel[1] = green;
+	pPixel[2] = red;
+}
+bool bBitMap::BitMap::GenerateBMP(std::string bmpFileName)
+{
+	BitMapFileHeader fileHeader;
+	fileHeader.fOffBits = sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader);
+	fileHeader.fSize = fileHeader.fOffBits + PixelsBites;
+	BitMapInfoHeader infoHeader;
+	infoHeader.Width = Width;
+	infoHeader.Height = Height;
+	std::ofstream bmpFile;
+	bmpFile.open(bmpFileName, std::ios::binary);
+	if (!bmpFile)
+		return false;
+	bmpFile.write((char*)&fileHeader, sizeof(fileHeader));
+	bmpFile.write((char*)&infoHeader, sizeof(infoHeader));
+	bmpFile.write((char*)Pixels.get(), PixelsBites);
+	bmpFile.close();
+	if (!bmpFile)
+		return false;
+	//alternative approach:
+	//https://stackoverflow.com/questions/2654480/writing-bmp-image-in-pure-c-c-without-other-libraries
+	return true;
+}
+
+bool tester_BitMap(int& numTests)
+{
+	{
+		bBitMap::BitMap bmp(800, 600);
+		for (int i = 0; i < 800; ++i)
+		{
+			int x = i;
+			int y = x / 2;
+			bmp.Draw1Pixel(x, y, 255, 0, 0);
+		}
+		bmp.GenerateBMP("..\\Data\\testBmp1.bmp");
+	}
+	++numTests;
+	return true;
+}
---
+++ BitMap.h
@@ -0,0 +1,45 @@
+#ifndef BITMAP_H
+#define BITMAP_H
+#include <string>
+#include <cstdint>
+#include <memory>
+#pragma pack(2)
+namespace bBitMap
+{
+	struct BitMapFileHeader
+	{
+		char HeaderBM[2]{ 'B', 'M' };
+		std::int32_t fSize;
+		std::int32_t Spacer{ 0 };
+		std::int32_t fOffBits;
+	}; 
+	struct BitMapInfoHeader
+	{
+		std::int32_t Size{ 40 };
+		std::int32_t Width;
+		std::int32_t Height;
+		std::int16_t Planes{ 1 };
+		std::int16_t BitsPerPixel{ 24 };
+		std::int32_t Compression{ 0 };
+		std::int32_t SizeImage{ 0 };
+		std::int32_t XRes{ 2400 };
+		std::int32_t YRes{ 2400 };
+		std::int32_t ClrUsed{ 0 };
+		std::int32_t ClrImportant{ 0 };
+	};
+	class BitMap
+	{
+		int Width{ 0 };
+		int Height{ 0 };
+		int PixelsBites{ 0 };
+		std::unique_ptr<uint8_t[]> Pixels{ nullptr };
+	public:
+		BitMap(int width, int height);
+		virtual ~BitMap();
+		void Draw1Pixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue);
+		bool GenerateBMP(std::string bmpFileName);
+	};
+}
+bool tester_BitMap(int& numTests);
+#endif
+
